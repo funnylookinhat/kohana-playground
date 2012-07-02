3b7cc6a1f0a4b18e389bb2c2b3f1077651ead9c8
David Overcash <funnylookinhat@gmail.com>
2012-07-02 14:45:13 -0600

    Updated test controller.

--- application/classes/controller/base.php
+++ application/classes/controller/base.php
@@ -3,67 +3,81 @@
 class Controller_Base extends Controller {
 
 	function action_index() {
-		
-		$playground_search = new Context_Playground_Search();
-		
-		// Test by_id
-		$result = $playground_search->by_id(1);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
-		echo '<br>';
-		
-		$result = $playground_search->by_id(2);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
+
+		$playground_search_id = new Playground_Playground_Search_Id(1);
+		$result = $playground_search_id->execute();
+		echo $this->dump_result($result);
 		echo '<br>';
 
-		$result = $playground_search->by_geo(0,0,0,0);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
+		$playground_search_id = new Playground_Playground_Search_Id(5);
+		$result = $playground_search_id->execute();
+		echo $this->dump_result($result);
 		echo '<br>';
 
-		$result = $playground_search->by_geo(5,15,0,40);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
-		echo '<br>';
-		
-		//
-		echo '<hr>';
-		$playground_search = new Context_Playground_Search(0,1);
-		$result = $playground_search->by_geo(-100,100,-100,100);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
-		echo ' ::: '.$result['playgrounds'][0]['name'];
-		echo '<br>';
-		
-		$playground_search = new Context_Playground_Search(1,1);
-		$result = $playground_search->by_geo(-100,100,-100,100);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
-		echo ' ::: '.$result['playgrounds'][0]['name'];
-		echo '<br>';
-		
-		$playground_search = new Context_Playground_Search(2,1);
-		$result = $playground_search->by_geo(-100,100,-100,100);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
-		echo ' ::: '.$result['playgrounds'][0]['name'];
-		echo '<br>';
-		
-		//
 		echo '<hr>';
 
-		$playground_search = new Context_Playground_Search(0,2,array('field' => 'id','direction' => 'DESC'));
-		$result = $playground_search->by_geo(-100,100,-100,100);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
-		echo ' ::: '.$result['playgrounds'][0]['name'].', '.$result['playgrounds'][1]['name'];
-		echo '<br>';
-		
-		$playground_search = new Context_Playground_Search(1,2,array('field' => 'id','direction' => 'DESC'));
-		$result = $playground_search->by_geo(-100,100,-100,100);
-		echo ( $result['success'] ? "Success: ".count($result['playgrounds']) : "Failure: ".$result['error'] );
-		echo ' ::: '.$result['playgrounds'][0]['name'].', '.$result['playgrounds'][1]['name'];
-		echo '<br>';
-		
+		$playground_search_name = new Playground_Playground_Search_Name('test');
+		$result = $playground_search_name->execute();
+		echo $this->dump_result($result);
+		echo '<hr>';
+
+		echo '<h2>Page Size: 2</h2>';
+
+		$playground_search_name = new Playground_Playground_Search_Name('test',0,2);
+		$result = $playground_search_name->execute();
+		echo $this->dump_result($result);
+		echo '<hr>';
+
+		$playground_search_name = new Playground_Playground_Search_Name('test',1,2);
+		$result = $playground_search_name->execute();
+		echo $this->dump_result($result);
+		echo '<hr>';
+
+		echo '<h2>Page Size: 3. Sort Order Reverse ID</h2>';
+
+		$playground_search_name = new Playground_Playground_Search_Name('test',0,3,array('field' => 'id','direction' => 'desc'));
+		$result = $playground_search_name->execute();
+		echo $this->dump_result($result);
+		echo '<hr>';
+
+		$playground_search_name = new Playground_Playground_Search_Name('test',1,3,array('field' => 'id','direction' => 'desc'));
+		$result = $playground_search_name->execute();
+		echo $this->dump_result($result);
+		echo '<hr>';
+
+		$playground_search_name = new Playground_Playground_Search_Name('this cannot be found');
+		$result = $playground_search_name->execute();
+		echo $this->dump_result($result);
+		echo '<hr>';
+
+		echo '<h2>GEO SEARCH SORT LAT ASC</h2>';
+
+		$playground_search_geo = new Playground_Playground_Search_Geo(4,15,0,100,0,50,array('field' => 'latitude','direction' => 'asc'));
+		$result = $playground_search_geo->execute();
+		echo $this->dump_result($result);
+		echo '<hr>';
+
 		die('done');
 
 	}
 
-	function action_create() {
-
+	private function dump_result($result) {
+		$string = '';
+		$string .= ( $result['success'] ? "SUCCESS" : "FAILURE" ).'<br>';
+		if( ! $result['success'] ) {
+			$string .= ( isset($result['error']) ? $result['error'] : 'UNKNOWN/UNCAUGHT ERROR' ).'<br>';
+		} else {
+			$string .=  "Playgrounds: ".count($result['playgrounds']).'<br>';
+			foreach( $result['playgrounds'] as $playground ) {
+				$string .=  '&nbsp;&nbsp;&nbsp;&nbsp;';
+				$string .=  $playground['id'].' ';
+				$string .=  $playground['name'].' ';
+				$string .=  $playground['latitude'].' ';
+				$string .=  $playground['longitude'].' ';
+				$string .=  '<br>';
+			}
+		}
+		return $string;
 	}
 
 }
